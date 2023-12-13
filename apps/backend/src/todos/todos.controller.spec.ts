bcafb02da61e0ed09ee98b39855543dd503f0fed
abde fakallah <senyoupl@icloud.com>
2023-12-13 19:47:01 +0100

    :white_check_mark: update test double logic

--- apps/backend/src/todos/todos.controller.spec.ts
+++ apps/backend/src/todos/todos.controller.spec.ts
@@ -3,49 +3,53 @@ import { TodosController } from './todos.controller';
 import { TodosService } from './todos.service';
 import { PrismaService } from '../prisma/prisma.service'
 import { ModuleMocker, MockFunctionMetadata } from 'jest-mock';
+import { createMock, DeepMocked } from '@golevelup/ts-jest';
+import { Todo } from '@prisma/client';
 
 
 describe('TodosController', () => {
   let controller: TodosController;
-  //let service: TodosService;
+  let service: DeepMocked<TodosService>;
+  let mydate: Date;
 
   beforeEach(async () => {
+
+    const mockDate = new Date();
+    jest.spyOn(global, "Date").mockImplementation(() => (mockDate as unknown) as Date);
+    mydate = new Date();
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [TodosController],
       //providers: [TodosService, PrismaService],
-    }).useMocker((token) => {
-      const results = ['test1', 'test2'];
-      if(token === TodosService) {
-        return { findAll: jest.fn().mockResolvedValue(results) };
-      }
-      if(token === PrismaService) {
-        return {
-          prisma: {
-            todo: {
-              findMany: jest.fn().mockResolvedValueOnce([
-                { 
-                  id: 1,
-                  title: 'sortir le chat',
-                  status: 'IN_PROGRESS',
-                  priority: 1,
-                  tags: ['animaux', 'sortie'],
-                  createdAt: '2023-11-12T22:49:00.000Z',
-                  updatedAt: '2023-12-12T22:49:00.000Z',
-                },
-              ])
-            }
-          }
-        }
-      }
-    })
+    }).useMocker(createMock)
     .compile();
 
     controller = module.get<TodosController>(TodosController);
-    // service = module.get<TodosService>(TodosService);
+    service = module.get(TodosService);
   });
 
-  it('findAll', () => {
+  it('find all todos', async () => {
       // jest.spyOn(service, 'findAll').mockImplementation(() => result);
-      expect(controller).toBeDefined();
+      service.findAll.mockResolvedValueOnce([{
+        id: 2,
+        title: "promener le chat",
+        status: "NOT_STARTED",
+        priority: 1,
+        tags: ['animaux', 'chat'],
+        createdAt: mydate,
+        updatedAt: mydate,
+      }]);
+
+      const allTodos = controller.findMany();
+
+      await expect(allTodos).resolves.toStrictEqual([{
+        id: 2,
+        title: "promener le chat",
+        status: "NOT_STARTED",
+        priority: 1,
+        tags: ['animaux', 'chat'],
+        createdAt: mydate,
+        updatedAt: mydate,
+      }]);
   });
 });
