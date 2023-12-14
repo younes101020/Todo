5cbdf867484b29d0b4182e61a40d0ed02fa17d72
abde fakallah <senyoupl@icloud.com>
2023-12-14 21:06:23 +0100

    :white_check_mark: setup test env on nextjs front + first sample test writting for todos

---
+++ apps/frontend/src/features/todos/__test__/index.test.tsx
@@ -0,0 +1,25 @@
+/**
+ * @jest-environment jsdom
+ */
+import { render, screen } from '@testing-library/react'
+import { Todo } from '@/features/todos/components/todo'
+
+describe('Home', () => {
+  it('renders a heading', () => {
+    render(
+        <Todo
+            title={"acheter des piles"}
+            tags={["tele", "confort"]}
+            status={"IN_PROGRESS"}
+            priority={"URGENT"}
+            createdAt={24}
+        />
+    )
+
+    const listitem = screen.getByRole('heading', {
+      name: "acheter des piles"
+    })
+
+    expect(listitem).toBeInTheDocument()
+  })
+})
