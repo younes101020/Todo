928191f55bb4d8a7efada1c774caab2f54aae90e
abde fakallah <senyoupl@icloud.com>
2023-12-16 17:55:04 +0100

    :alien: handle server error case and fix nextjs cache abuse

--- apps/frontend/src/features/todos/components/todos.tsx
+++ apps/frontend/src/features/todos/components/todos.tsx
@@ -15,24 +15,27 @@ const Todos = () => {
         isPlaceholderData,
     } = useQuery({ 
         queryKey: useTodoQueryKey.all,
-        queryFn: () => getTodos,
+        queryFn: () => getTodos(),
         placeholderData: keepPreviousData,
     });
-
+  
     return (
         <div className="flex justify-center py-5">
             <ul className="w-80 flex flex-col gap-5">
                 {
-                    data?.map(({ id, title, status, priority, tags, createdAt }: TodoType) => {
-                        return <Todo 
-                                    key={id} 
-                                    title={title}
-                                    status={status}
-                                    priority={priority}
-                                    tags={tags}
-                                    createdAt={createdAt}
-                                />
-                    })
+                    data ? 
+                        data.map(({ id, title, status, priority, tags, createdAt }: TodoType) => {
+                            return <Todo 
+                                        key={id} 
+                                        title={title}
+                                        status={status}
+                                        priority={priority}
+                                        tags={tags}
+                                        createdAt={createdAt}
+                                    />
+                        }) 
+                        : 
+                        "server error component"
                 }
             </ul>
             
