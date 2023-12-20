4d5b1cbd8d331cab0faa58cef829733f17975cba
abde fakallah <senyoupl@icloud.com>
2023-12-20 20:35:45 +0100

    :construction: WIP let axios handle fetching logic

---
+++ apps/frontend/src/features/todos/api/http-common.ts
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const apiClient = axios.create({
+  baseURL: "http://localhost:3001/todos"
+});
+
+// Next work on handling global /todos verification or error case using this middleware axios callback:
+
+// axios.interceptors.response.use(function (response) {
+//     console.log(response)
+//     return response;
+// }, function(error) {
+//     console.log(error)
+//     return Promise.reject(error);
+// });
+
+export { apiClient };
